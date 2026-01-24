f44627aa8078a9b84aa367405cf6ed1954850ae8
deniska69 <gostev.dy@yandex.ru>
2026-01-24 14:56:47 +0700

    upp layout and header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,40 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "@/styles/globals.css";
-import LayoutHeader from "@/components/layout/Header";
-import { Providers } from "./providers";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import '@/styles/globals.css';
+import LayoutHeader from '@/components/layout/Header';
+import { Providers } from './providers';
+import { Div } from '@/components/ui';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "WatchedIt",
-  description: "Самые честные отзывы об играх, фильмах, сериалах и книгах",
+  title: 'WatchedIt',
+  description: 'Самые честные отзывы об играх, фильмах, сериалах и книгах',
 };
 
-interface IRootLayout {
+interface ILayout {
   children: React.ReactNode;
 }
 
-export default function RootLayout({ children }: IRootLayout) {
+export default function Layout({ children }: ILayout) {
   return (
     <html lang="ru">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         <Providers>
           <LayoutHeader />
-          {children}
+          <Div className="flex min-h-[calc(100svh-var(--navbar-height))] justify-center font-sans">
+            <main className="flex w-full max-w-5xl p-6 pb-0">{children}</main>
+          </Div>
         </Providers>
       </body>
     </html>
--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -13,19 +13,18 @@ import {
 import { usePathname } from 'next/navigation';
 import ThemeSwitch from './ThemeSwitch';
 import { Stack } from '@/components/ui';
+import { Fragment } from 'react';
 
-const AcmeLogo = () => {
-  return (
-    <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
-      <path
-        clipRule="evenodd"
-        d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-        fill="currentColor"
-        fillRule="evenodd"
-      />
-    </svg>
-  );
-};
+const AcmeLogo = () => (
+  <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
+    <path
+      clipRule="evenodd"
+      d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
+      fill="currentColor"
+      fillRule="evenodd"
+    />
+  </svg>
+);
 
 const menuItems = [
   {
@@ -43,54 +42,39 @@ const menuItems = [
 ];
 
 export default function LayoutHeader() {
-  const pathname = usePathname();
   return (
     <Navbar disableAnimation isBordered isBlurred={false}>
-      <NavbarContent className="sm:hidden" justify="start">
+      <MobileHeader />
+      <DesktopHeader />
+    </Navbar>
+  );
+}
+
+const MobileHeader = () => {
+  const pathname = usePathname();
+
+  return (
+    <Fragment>
+      <NavbarContent className="md:hidden" justify="start">
         <NavbarMenuToggle />
       </NavbarContent>
 
-      <NavbarContent className="pr-3 sm:hidden" justify="center">
-        <NavbarBrand>
+      <NavbarContent className="md:hidden" justify="center">
+        <NavbarBrand className="mr-4 text-white" as={Link} href="/">
           <AcmeLogo />
           <p className="font-bold text-inherit">ACME</p>
         </NavbarBrand>
       </NavbarContent>
 
-      <NavbarContent className="hidden gap-4 sm:flex" justify="center">
-        <NavbarBrand>
-          <AcmeLogo />
-          <p className="font-bold text-inherit">ACME</p>
-        </NavbarBrand>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Features
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link aria-current="page" color="warning" href="#">
-            Customers
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
-
-      <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
-        </NavbarItem>
+      <NavbarContent className="md:hidden" justify="end">
         <NavbarItem>
           <Button as={Link} color="warning" href="#" variant="flat">
-            Sign Up
+            Войти
           </Button>
         </NavbarItem>
       </NavbarContent>
 
-      <NavbarMenu className="h-full max-h-[calc(100%-var(--navbar-height))] pb-20">
+      <NavbarMenu className="h-full max-h-[calc(100%-var(--navbar-height))] pt-6 pb-20">
         <Stack className="h-full justify-between">
           <Stack className="gap-y-3">
             {menuItems.map((item, index) => (
@@ -110,6 +94,44 @@ export default function LayoutHeader() {
           <ThemeSwitch />
         </Stack>
       </NavbarMenu>
-    </Navbar>
+    </Fragment>
   );
-}
+};
+
+const DesktopHeader = () => {
+  const pathname = usePathname();
+
+  return (
+    <Fragment>
+      <NavbarContent className="hidden gap-4 md:flex" justify="center">
+        <NavbarBrand className="mr-4 text-white" as={Link} href="/">
+          <AcmeLogo />
+          <p className="font-bold text-inherit">ACME</p>
+        </NavbarBrand>
+
+        {menuItems.map((item, index) => (
+          <NavbarItem key={`${item.title}-${index}`}>
+            <Link
+              color={pathname === item.href ? 'warning' : 'foreground'}
+              href={item.href}
+              size="lg"
+            >
+              {item.title}
+            </Link>
+          </NavbarItem>
+        ))}
+      </NavbarContent>
+
+      <NavbarContent className="hidden md:flex" justify="end">
+        <NavbarItem className="hidden text-white lg:flex">
+          <Link href="#">Войти</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="warning" href="#" variant="flat">
+            Регистрация
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Fragment>
+  );
+};
